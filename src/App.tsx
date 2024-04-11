5f364a1ff85461b262db3173cb392643d180eb7e
genics85 <eugeneamo85@gmail.com>
2024-04-11 23:58:19 +0000

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import NavigationBar from "./components/NavigationBar";
+
+function App() {
+  return (
+    <div className=" min-h-screen flex flex-col bg-gradient-to-b from-[#030012] via-[#0e042d] via-90% to-[#030012]">
+      <NavigationBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,30 @@
+import logo from "../assets/uniloot_logo.svg";
+import avatar from "../assets/avatar.png"
+
+function NavigationBar() {
+  return (
+    <div className="container mt-5 mx-auto text-white flex items-center justify-between">
+      <div className="flex gap-3 items-center">
+        <div>
+          <img src={logo} alt="" />
+        </div>
+        <p className="font-semibold text-[20px]">
+          UNILLOT<span className="text-[#c61ed4]">B</span>OT
+        </p>
+      </div>
+      <div className="flex gap-12 items-center">
+        <button className="bg-gradient-to-r py-2 px-6 rounded-md text-[14px] from-[#9551f0] to-[#c61ed4] ">
+          Refferal link
+        </button>
+        <div className="flex gap-2">
+            <div>
+                <img src={avatar} alt="" />
+            </div>
+            <p className="text-[16px] ">Andy</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default NavigationBar;
